a1f01a143b646c1bb13ea179b183bf7691115676
Erdogan Taskesen <erdogant@gmail.com>
2020-09-07 22:43:14 +0200

    update files

--- pypi_downloads/pypi_downloads.py
+++ pypi_downloads/pypi_downloads.py
@@ -16,13 +16,14 @@ curpath = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data')
 
 class pypi_downloads:
 
-    def __init__(self, username, sep=';', verbose=3):
+    def __init__(self, username, category=['with_mirrors', 'without_mirrors'], sep=';', verbose=3):
         self.username = username
         self.repo_link = 'https://api.github.com/users/' + username + '/repos'
         self.sep = sep
+        self.category = category
         self.verbose=verbose
 
-    def get_repo_names(self):
+    def get_repo_names_from_git(self):
         # Extract repos for user
         if self.verbose>=3: print('[pypi_downloads] >Extracting repo names for [%s]..' %(self.username))
         r = requests.get(self.repo_link)
@@ -39,7 +40,7 @@ class pypi_downloads:
 
     def update(self, repo=None):
         # Extract all repos
-        repos = self.get_repo_names()
+        repos = self.get_repo_names_from_git()
         # Check whether specific repo exists.
         if repo is not None:
             if not np.any(np.isin(repos, repo)): raise ValueError('[pypi_downloads] >Error: repos [%s] does not exists or is private.' %(repo))
@@ -56,16 +57,13 @@ class pypi_downloads:
                 # Merge with any on disk
                 pathname = os.path.join(curpath, repo + '.csv')
                 if os.path.isfile(pathname):
-                    if self.verbose>=3: print('[pypi_downloads] >[%s] is also found on disk. Merge -> Update -> Write' %(repo))
                     # Read repo from disk
-                    df_disk = read_repo_from_disk(pathname, self.sep)
+                    df_disk = read_repo_counts_from_disk(pathname, self.sep)
                     # Merge with latest counts
-                    df = pd.concat([df, df_disk], axis=0)
-                    df.drop_duplicates(inplace=True)
-                    df = df.sort_values("date")
-                    df.reset_index(drop=True, inplace=True)
-
-                df.to_csv(pathname, index=False, sep=self.sep)
+                    df, status = add_new_counts_to_repo(df, df_disk, repo, verbose=self.verbose)
+                # Write to disk
+                if status:
+                    df.to_csv(pathname, index=False, sep=self.sep)
             except:
                 if self.verbose>=1: print('[pypi_downloads] >Skip [%s] coz not exists on pypi.' %(repo))
 
@@ -97,32 +95,39 @@ class pypi_downloads:
             repos = repos[Iloc]
             filenames = filenames[Iloc]
             pathnames = pathnames[Iloc]
-    
+
         if not status:
             if self.verbose>=3: print('[pypi_downloads] >No repos could be retrieved from git nor disk <return>')
             return None
-        
-        fig, ax = plt.subplots(figsize=(10, 2))
+
+        # fig, ax = plt.subplots(figsize=(10, 2))
         out = pd.DataFrame()
         for repo, pathname in zip(repos, pathnames):
-            df = read_repo_from_disk(pathname, self.sep)
+            df = read_repo_counts_from_disk(pathname, self.sep)
+
+            # Take desired categories
+            Iloc = np.isin(df['category'], self.category)
+            df = df.loc[Iloc, :]
+
+            # Group by date
             df = df.groupby("date").sum().sort_values("date")
             df.reset_index(drop=False, inplace=True)
 
             dftmp = df.groupby("date").sum()
-            dftmp.rename(columns = {'downloads': repo}, inplace=True)
+            dftmp.rename(columns = {'downloads' : repo}, inplace=True)
             out = pd.concat([out, dftmp], axis=0)
-            
-            df['weeknr'] = df['date'].dt.week
-            df['cumsum'] = df['downloads'].cumsum()
-            ax.plot(df['weeknr'].values, df['cumsum'].values, label=repo)
+
+            # df['weeknr'] = df['date'].dt.week
+            # df['cumsum'] = df['downloads'].cumsum()
+            # ax.plot(df['weeknr'].values, df['cumsum'].values, label=repo)
             # ax.plot(df['weeknr'].values, df['downloads'].values, label=repo)
-        
+
         out.fillna(value=0, inplace=True)
+        self.results = out
         # out.plot()
 
-        ax.legend()
-        ax.grid(True)
+        # ax.legend()
+        # ax.grid(True)
 
         # data = pypistats.overall("pillow", total=True, format="pandas")
         # df = df.groupby("category").get_group("without_mirrors").sort_values("date")
@@ -132,7 +137,10 @@ class pypi_downloads:
         # chart = df.plot(x="date", y="downloads", figsize=(10, 2))
         # chart.figure.show()
         # chart.figure.savefig("overall.png")  # alternatively
-        return out
+        return self.results
+    
+    def plot():
+        pass
 
 # %%
 def get_files_on_disk(verbose=3):
@@ -148,10 +156,25 @@ def get_files_on_disk(verbose=3):
     pathnames = np.array(list(map(lambda x: os.path.join(curpath, x), filenames)))
     return repos, filenames, pathnames 
 
-def read_repo_from_disk(pathname, sep):
+def read_repo_counts_from_disk(pathname, sep):
     df_disk = pd.read_csv(pathname, sep=sep)
     df_disk['date'] = pd.to_datetime(df_disk['date'], format='%Y-%m-%d')
     return df_disk
+
+def add_new_counts_to_repo(df, df_disk, repo, verbose=3):
+    STATUS = False
+    count_before = df.shape[0]
+    df = pd.concat([df, df_disk], axis=0)
+    df.drop_duplicates(inplace=True)
+    df = df.sort_values("date")
+    df.reset_index(drop=True, inplace=True)
+
+    count_after = df.shape[0]
+    if count_after>count_before:
+        STATUS=True
+        if verbose>=3: print('[pypi_downloads] >[%s] updated.' %(repo))
+
+    return df, STATUS
 
 # %% Main
 # if __name__ == "__main__":
