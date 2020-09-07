2003597e40ec5379699dee9a9f6d7784ce3884b0
Erdogan Taskesen <erdogant@gmail.com>
2020-09-07 22:43:24 +0200

    example update

--- pypi_downloads/examples.py
+++ pypi_downloads/examples.py
@@ -1,5 +1,4 @@
 # %%
-
 # import pypi_downloads
 # print(pypi_downloads.__version__)
 
@@ -7,16 +6,19 @@ from pypi_downloads import pypi_downloads
 print(dir(pypi_downloads))
 
 # %%
+pp = pypi_downloads(username='erdogant', category='with_mirrors')
+pp.update()
+
+# %%
 pp = pypi_downloads(username='erdogant')
-# pp.update()
 pp.stats(repo='bnlearn')
 pp.stats(repo='distfit')
 pp.stats(repo='worldmap')
 pp.stats(repo='hnet')
 pp.stats(repo='ismember')
-pp.stats(repo='ismember')
 pp.stats(repo='flameplot')
 pp.stats(repo='pca')
-pp.stats(repo='df2onehot')
+pp.stats(repo=['df2onehot','pca','bnlearn','ismember','thompson'])
+pp.stats()
 
 # %%
