ab539c93b895f580c552214aaed5f64668e303df
Erdogan Taskesen <erdogant@gmail.com>
2020-09-15 22:08:39 +0200

    download and push to git

--- run_store.sh
+++ run_store.sh
@@ -1,14 +1,18 @@
 echo "Run pypiplot.."
 python pypiplot/pypiplot.py -u 'erdogant' -p 'D://PY/REPOSITORIES/erdogant.github.io/docs/imagesc/pypi/pypi_heatmap.html'
-read -p "Press [Enter] key to close window..."
+
+echo ""
+read -p "Press [Enter] to push to Git.."
 
 echo ""
 echo "Go to dir: erdogant.github.io"
 cd D:\PY\REPOSITORIES\erdogant.github.io
 
 echo ""
-echo "Push to git"
 git pull
 git add .
 git commit -m "update pypiplot"
 git push
+
+echo ""
+read -p "Press [Enter] to close"
