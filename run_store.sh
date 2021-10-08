0c3aa23647dad3c4d1a56c8708fe6d18da90c393
erdogant <erdogant@gmail.com>
2021-10-08 11:11:21 +0200

    update example files

--- run_store.sh
+++ run_store.sh
@@ -1,14 +1,14 @@
-cd D://PY/REPOSITORIES/pypiplot
+cd D://REPOS/pypiplot
 
 echo "Run pypiplot.."
-python pypiplot/pypiplot.py -u 'erdogant' -p 'D://PY/REPOSITORIES/erdogant.github.io/docs/imagesc/pypi/pypi_heatmap.html' -v '700'
+python pypiplot/pypiplot.py -u 'erdogant' -p 'D://REPOS/erdogant.github.io/docs/imagesc/pypi/pypi_heatmap.html' -v '700'
 
 echo ""
 read -p "Press [Enter] to push to Git.."
 
 echo ""
 echo "Go to dir: erdogant.github.io"
-cd D://PY/REPOSITORIES/erdogant.github.io
+cd D://REPOS/erdogant.github.io
 
 echo ""
 git pull
